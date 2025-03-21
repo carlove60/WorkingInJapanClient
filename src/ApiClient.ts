05a1ebd82b11208c358b329fdf87c0912fc1eea3
carlove <carlove60@hotmail.com>
2025-03-21 07:56:56 +0100

    clean up

--- src/ApiClient.ts
+++ src/ApiClient.ts
@@ -2,13 +2,9 @@ import {ApiClient, createApiClient} from "../generated-client/apiClient.ts";
 import { AuthMiddleware } from "./Middleware/AuthMiddleware.ts";
 import {AuthenticationProvider, RequestInformation} from "@microsoft/kiota-abstractions";
 import {FetchRequestAdapter, HttpClient} from "@microsoft/kiota-http-fetchlibrary";
+import {isNullOrUndefined} from "./Helpers/Guard.ts";
 
-class SimpleAuthenticationProvider
-    implements AuthenticationProvider {
-    public async getAuthorizationToken(): Promise<string | undefined> {
-        return localStorage.getItem("token") ?? undefined;
-    }
-
+class SimpleAuthenticationProvider implements AuthenticationProvider {
     authenticateRequest(request: RequestInformation, additionalAuthenticationContext: Record<string, unknown> | undefined): Promise<void> {
         if (request && additionalAuthenticationContext) {
             return Promise.resolve(undefined);
@@ -22,10 +18,8 @@ class Client {
     private static _instance: ApiClient;
 
     static get instance(): ApiClient {
-        if (this._instance === undefined) {
+        if (isNullOrUndefined(this._instance)) {
             const httpClient = new HttpClient(undefined, new AuthMiddleware());
-
-
             const adapter = new FetchRequestAdapter(new SimpleAuthenticationProvider(), undefined, undefined, httpClient);
             this._instance = createApiClient(adapter);
         }
