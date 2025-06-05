7d627fa1ae61bdf223c412ca4b4bcb7d287fe67e
carlove <carlove60@hotmail.com>
2025-06-05 20:48:17 +0200

    Unnecessary code removed

---
+++ src/Components/AddToWaitingList/styles.ts
@@ -0,0 +1,8 @@
+import * as React from "react";
+
+export const joinWaitList: React.CSSProperties = {
+  flexDirection: "row",
+  justifyContent: "space-between",
+  alignSelf: "center",
+  paddingTop: 25,
+};
